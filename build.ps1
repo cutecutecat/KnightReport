37f6a682f8f9d25fc659691a4d712b1621904c2b
cutecutecat <starkind1997@gmail.com>
2021-09-29 14:55:33 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,8 @@
-rm -r -Force ./tmp
+$OUTPUT_PATH="./tmp"
+$EXIST=Test-Path $OUTPUT_PATH
+if ($EXIST -eq "True"){
+  rm -r -Force ./tmp
+}
 mkdir tmp
 cd tmp
 pyinstaller -Fw ../src/main.py ../src/ctrl.py ../src/constants.py ../src/utils.py -n KnightReport
