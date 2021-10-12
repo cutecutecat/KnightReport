e16dff513facb2c7087c890a66ff7d8e74df5321
陈俊羽 <kind@mail.ustc.edu.cn>
2021-10-12 16:30:44 +0800

    fix testcase

--- testcase/main_test.py
+++ testcase/main_test.py
@@ -1,5 +1,6 @@
 import sys
 import unittest
+from http import cookiejar
 from os.path import dirname, join
 
 sys.path.append(dirname(__file__))
@@ -12,14 +13,15 @@ from testcase.data import TestData
 class TestCtrl(unittest.TestCase):
     def setUp(self):
         self.data = TestData()
+        self.blankCookiesJar = cookiejar.CookieJar()
 
     def test_extract_guild(self):
-        ctrl = Ctrl()
+        ctrl = Ctrl(self.blankCookiesJar)
         ctrl.extract_guild_info(self.data.guild_data)
         self.assertEqual(ctrl.uid_name, dict(zip(self.data.ids, self.data.names)))
         self.assertEqual(ctrl.all_uid, set(self.data.ids))
 
     def test_extract_attack(self):
-        ctrl = Ctrl()
+        ctrl = Ctrl(self.blankCookiesJar)
         ctrl.extract_guild_info(self.data.guild_data)
         ctrl.extract_date_info(0, self.data.attack_data)
